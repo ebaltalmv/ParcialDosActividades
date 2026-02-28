7af1f65148ff44d45c07079338632c67fdcb3338
Emmanuel <emma.balta09@gmail.com>
2026-02-28 12:15:45 -0600

    add logic for labes and sliders

---
+++ DataBindingExample/DataBindingExample/Pages/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DataBindingExample.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
