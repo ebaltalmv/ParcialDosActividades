cf0b7aee892bc473f54a3d18aa3b2758cb19e6ed
Emmanuel <emma.balta09@gmail.com>
2026-03-21 11:54:28 -0600

    Mejorar la edición y guardado de aerolíneas

--- DataBindingExample/DataBindingExample/ViewModels/AirlineViewModel.cs
+++ DataBindingExample/DataBindingExample/ViewModels/AirlineViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DataBindingExample.Models;
 using System.Text.RegularExpressions;
 
 namespace DataBindingExample.ViewModels
@@ -33,6 +35,50 @@ namespace DataBindingExample.ViewModels
             IsCodeValid =
                  !string.IsNullOrWhiteSpace(value) &&
                  Regex.IsMatch(value, "^[A-Za-z0-9]{2,}$");
+        }
+
+        /// <summary>
+        /// Invoked when the value of the name property changes, allowing for custom logic to be executed in response to
+        /// the change.
+        /// </summary>
+        /// <param name="value">The new value assigned to the name property.</param>
+        partial void OnNameChanged(string value)
+        {
+            ValidateName(value);
+        }
+
+        private void ValidateName(string value)
+        {
+            IsNameValid = !string.IsNullOrWhiteSpace(value);
+        }
+
+        [RelayCommand]
+        public async Task SaveAirline()
+        {
+            var isEditMode = AirlineModel.Airlines.SingleOrDefault(a => a.Code == this.Code);
+            if (isEditMode != null)
+            {
+                await Shell.Current.DisplayAlertAsync("Edit Mode", "This is edition mode", "OK");
+                return;
+            }
+            AirlineModel airline = new()
+            {
+                Code = this.Code,
+                Name = this.Name,
+                Country = this.Country,
+                Phone = this.Phone
+            };
+
+            AirlineModel.Airlines.Add(airline);
+            await Shell.Current.GoToAsync("..");
+        }
+
+        public void LoadAirlineForEdition(AirlineModel airline)
+        {
+            this.Code = airline.Code;
+            this.Name = airline.Name;
+            this.Country = airline.Country;
+            this.Phone = airline.Phone;
         }
     }
 }
--- DataBindingExample/DataBindingExample/Views/AddAirlinePage.xaml.cs
+++ DataBindingExample/DataBindingExample/Views/AddAirlinePage.xaml.cs
@@ -1,12 +1,29 @@
+using DataBindingExample.Models;
 using DataBindingExample.ViewModels;
 
 namespace DataBindingExample.Views;
 
+[QueryProperty("Airline", "Airline")]
 public partial class AddAirlinePage : ContentPage
 {
+    private AirlineModel _airline;
+    private readonly AirlineViewModel _airlineViewModel;
     public AddAirlinePage()
     {
         InitializeComponent();
-        BindingContext = new AirlineViewModel();
+        _airlineViewModel = new AirlineViewModel();
+        BindingContext = _airlineViewModel;
+    }
+
+    public AirlineModel Airline
+    {
+        get => _airline; set
+        {
+            _airline = value;
+            if (_airlineViewModel != null && value != null)
+            {
+                _airlineViewModel.LoadAirlineForEdition(value);
+            }
+        }
     }
 }
